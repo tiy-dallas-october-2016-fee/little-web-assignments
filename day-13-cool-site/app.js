8b16f1d9f411509c565defae6035aeb29a193b9f
Eric Sowell <eric.sowell@gmail.com>
2016-12-01 16:01:58 -0600

    some updates

--- day-13-cool-site/app.js
+++ day-13-cool-site/app.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.use(express.static('public'))
 
-// app.get('/hello', function(req, res) {
-//   res.send('hello friend');
-// });
+app.get('/hello', function(req, res) {
+  res.send('hello friend');
+});
 
 app.listen(3001, function() {
   console.log('Listening on port 3001');
