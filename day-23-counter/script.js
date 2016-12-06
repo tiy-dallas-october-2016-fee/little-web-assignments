01a4d06852e4a08e4031836f1f0871cdaf7ba7da
Eric Sowell <eric.sowell@gmail.com>
2016-12-06 09:54:37 -0600

    change day 23 assignment to use local storage

--- day-23-counter/script.js
+++ day-23-counter/script.js
@@ -1,6 +1,13 @@
 (function() {
 
+  var localStorageValue = localStorage.getItem('counter');
+  console.log('here it is, yo', localStorageValue);
+
   var counter = 0;
+  if (localStorageValue !== null) {
+    counter = Number(localStorageValue);
+  }
+
   var decrementButton = document.querySelector('.decrement');
   var incrementButton = document.querySelector('.increment');
   var display = document.querySelector('.display');
@@ -10,11 +17,13 @@
   decrementButton.addEventListener('click', function() {
     counter--;
     display.textContent = counter;
+    localStorage.setItem('counter', counter);
   });
 
   incrementButton.addEventListener('click', function() {
     counter++;
     display.textContent = counter;
+    localStorage.setItem('counter', counter);
   });
 
 })();
