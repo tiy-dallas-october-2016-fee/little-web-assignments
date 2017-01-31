ca173ac1f623b1d26d57b8faa9faa16545c967ac
Eric Sowell <eric.sowell@gmail.com>
2017-01-31 10:40:00 -0600

    youtube example

--- final-weeks-youtube/script.js
+++ final-weeks-youtube/script.js
@@ -9,10 +9,30 @@ if (this.YT === undefined) this.YT = {};
     console.log('starting!', context);
 
     $.ajax({
-      url: 'https://www.googleapis.com/youtube/v3/search?q=minecraft&key=keygoeshere&part=snippet'
+      url: 'https://www.googleapis.com/youtube/v3/search?q=minecraft&key={your-key-here}&part=snippet'
     })
     .done(function(data) {
       console.log('Look at this --> ', data);
+
+      var outputUL = document.querySelector('#output');
+
+      data.items.forEach((item) => {
+        console.log(item);
+
+        var li = document.createElement('li');
+
+        var img = document.createElement('img');
+        img.src = item.snippet.thumbnails.medium.url;
+        li.appendChild(img);
+
+        var channel = document.createElement('a');
+        channel.href = 'https://www.youtube.com/channel/' + item.snippet.channelId;
+        channel.textContent = item.snippet.channelTitle;
+        li.appendChild(channel);
+
+        outputUL.appendChild(li);
+      });
+
     });
 
   }
